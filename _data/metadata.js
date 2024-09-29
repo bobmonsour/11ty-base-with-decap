040bb74e77a54d50344f572da98cf812b5c18233
Bob Monsour <bob.monsour@gmail.com>
2024-09-29 14:27:13 -0700

    Added Decap CMS files

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,11 +1,11 @@
 module.exports = {
-	title: "Eleventy Base Blog v8",
-	url: "https://example.com/",
-	language: "en",
-	description: "I am writing about my experiences as a naval navel-gazer.",
-	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
-	}
-}
+  title: "Messing with Decap CMS and the Eleventy Base Blog v8",
+  url: "https://11ty-base-with-decap.pages.dev/",
+  language: "en",
+  description: "I am writing about my experiences as a naval navel-gazer.",
+  author: {
+    name: "Bob Monsour",
+    email: "bob.monsour@gmail.com",
+    url: "https://11ty-base-with-decap.pages.dev/",
+  },
+};
